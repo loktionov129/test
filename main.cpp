1665262b8f4d1593d9c92826717ac904ddae23f3
Alex Ander <loktionov129@gmail.com>
2017-06-17 14:47:15 +0700

    How do you do?

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     std::cout << "Hello Qt!" << std::endl;
+    std::cout << "How do you do?" << std::endl;
 
     return a.exec();
 }
